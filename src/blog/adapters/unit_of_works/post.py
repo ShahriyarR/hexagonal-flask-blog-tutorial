267ee78ef109221958577d223a4e4895d2dba8b6
Shako Rzayev <shahriyar.rzayev@nordsec.com>
2023-05-14 19:24:07 +0200

    feat: add uow for post service

---
+++ src/blog/adapters/unit_of_works/post.py
@@ -0,0 +1,24 @@
+from typing import Any, Callable
+
+from blog.adapters.repositories.post import PostRepository
+from blog.domain.ports.unit_of_works.post import PostUnitOfWorkInterface
+
+
+class PostUnitOfWork(PostUnitOfWorkInterface):
+    def __init__(self, session_factory: Callable[[], Any]):
+        self.session_factory = session_factory()
+
+    def __enter__(self):
+        self.session = self.session_factory()
+        self.calculation = PostRepository(self.session)
+        return super().__enter__()
+
+    def __exit__(self, *args):
+        super().__exit__(*args)
+        self.session.close()
+
+    def _commit(self):
+        self.session.commit()
+
+    def rollback(self):
+        self.session.rollback()
